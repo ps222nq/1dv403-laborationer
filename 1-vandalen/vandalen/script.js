d0ac0951bd8088bcc95bb1f0ce9bd7056b8b1212
ps222nq <ps222nq@student.lnu.se>
2014-11-12 20:48:54 +0100

    swedish sorting
    added localeCompare to sort() so non-english characters would sort

--- 1-vandalen/vandalen/script.js
+++ 1-vandalen/vandalen/script.js
@@ -5,16 +5,18 @@ var makePerson = function(persArr){
 //extract names and ages from persArr
 var ages = persArr.map(function(age){return age.age;});
 ages.sort();
-var names = persArr.map(function(name){return name.name;});
-names.sort();
 
-//debug
+var names = persArr.map(function(name){return name.name;});
+names.sort(function(a,b){return a.localeCompare(b, "sv")}); //localeCompare so sorting works w/ swedish characters too
+
+/*debug
 console.log(ages[0]);
 console.log(ages[1]);
 console.log(ages[2]);
 console.log(names[0]);
 console.log(names[1]);
 console.log(names[2]);
+*/
 
 //calculate minage, maxage, averageage, create names string
 
@@ -23,13 +25,13 @@ var maxAge = ages[ages.length - 1];
 var averageAge = Math.round((ages.reduce(function(a,b){return a + b;})) / ages.length);
 var stringOfNames = names.join(", ");
 
-//debug
+/*debug
 console.log("min " + minAge);
 console.log("max " + maxAge);
 console.log("avg " + averageAge);
-console.log(stringOfNames);
+console.log(stringOfNames); */
 
-//return object containing minAge int, maxAge int, averageAge int, names string sep by ", " and a-z sorted
+//return object containing minAge, maxAge, averageAge, names
 
 var res = {
 	minAge : minAge,
